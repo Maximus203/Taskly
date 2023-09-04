8128186d0d3bd759a33643b4abced9b864dcbed2
Maximus203 <printf0cherif@gmail.com>
2023-09-04 00:06:45 +0000

    Acces au fichier static

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/static/uploads', express.static('./uploads'));
 
 app.get('/taskly-api', (req, res) => {
     res.status(200).json({
